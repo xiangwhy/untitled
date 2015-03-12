46896c5c7bcd2a65d931a59f768a79ae7447c84d
xiang_000 <xiangwhy@163.com>
2015-03-12 23:08:39 +0800

    ok

---
+++ ex03.py
@@ -0,0 +1,11 @@
+__author__ = 'xiang_000'
+print("I will now count my chickens:")
+print("Hens", 100 - 25 * 3 % 4)
+
+print("Now I will count the eggs:")
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 -7?")
+
+print(3 + 2 < 5 - 7)
