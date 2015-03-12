c286f263f6d99c2b7b4de56cd09c4b703f97fc80
xiang_000 <xiangwhy@163.com>
2015-03-12 20:59:30 +0800

    ok

--- 02.py
+++ 02.py
@@ -1,7 +1,18 @@
-word = 'python'
-word[0] = '1'
-word = str(word)
-print(word)
-print(word)
-print('fff')
-print('fff')
+import smtplib
+
+sender = '240451@qq.com'
+receivers = ['240451@qq.com']
+
+message = """From: From Person <from@fromdomain.com>
+To: To Person <to@todomain.com>
+Subject: SMTP e-mail test
+
+This is a test e-mail message.
+"""
+
+try:
+    smtpObj = smtplib.SMTP('localhost')
+    smtpObj.sendmail(sender, receivers, message)
+    print("Successfully sent email")
+except:
+    print("Error: unable to send email")
