bf8044617ed03f1697bb7567f0e390e31904ebde
xiang_000 <xiangwhy@163.com>
2015-03-13 15:26:04 +0800

    ok

---
+++ ex08.py
@@ -0,0 +1,14 @@
+__author__ = 'xiang_000'
+
+formatter = "%r %r %r %r"
+
+print(formatter % (1, 2, 3, 4))
+print(formatter % ("one", "two", "three", "four"))
+print(formatter % (True, False, False, True))
+print(formatter % (formatter, formatter, formatter, formatter))
+print(formatter % (
+    "Ihad this thing.",
+    "That you could type up right.",
+    "But it didn't sing.",
+    "So I said goodnight."
+                  ))
