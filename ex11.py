27fa5192d67ce866b21f46c2a3ea08cd2d4c152d
xiang_000 <xiangwhy@163.com>
2015-03-13 16:34:29 +0800

    ok

--- ex11.py
+++ ex11.py
@@ -1,1 +1,11 @@
 __author__ = 'xiang_000'
+
+print("How old are you?")
+age = input()
+print("How tall are you?")
+height = input()
+print("How much do you weigh?")
+weight = input()
+
+print("So,you're %r old. %r tall and %r heavy." % (age, height, weight))
+
