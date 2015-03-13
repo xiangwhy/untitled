c0202b59677d89554494d8c89dd735f92d933152
xiang_000 <xiangwhy@163.com>
2015-03-13 16:51:53 +0800

    ok

--- ex12.py
+++ ex12.py
@@ -1,1 +1,8 @@
 __author__ = 'xiang_000'
+
+age = input("How old are you?")
+height = input("How tall are you?")
+weight = input("How much do you weigh")
+
+print("So,you're %r old, %r tall and %r heavy." % (age, height, weight))
+
---
+++ ex13.py
@@ -0,0 +1,10 @@
+__author__ = 'xiang_000'
+
+from sys import argv
+
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("You third variable is:", third)
