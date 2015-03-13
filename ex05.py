26e903cbffc3274b77de6c944ceb24bf82fe6b49
xiang_000 <xiangwhy@163.com>
2015-03-13 10:51:11 +0800

    ok

--- ex05.py
+++ ex05.py
@@ -1,3 +1,19 @@
 __author__ = 'xiang_000'
 
+my_name = "Zed A. shaw"
+my_age = 35  # not a lie
+my_height = 74  # inches
+my_weight = 180  # lbs
+my_eyes = "Blue"
+my_teeth = "White"
+my_hair = "Brown"
 
+print("Let's tali about %s." % my_name)
+print("He's %d inches tall." % my_height)
+print("He' %d pounds heavy." % my_weight)
+print("Actually that 's not too heavy.")
+print("He's got %s eyes and %s hait." % (my_eyes, my_hair))
+print("His teeth are usually %s depending on the cooffee.好的" % my_teeth)
+
+# this line is tricky,try to get it exactly right
+print("If I add %d , %d , and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight))
